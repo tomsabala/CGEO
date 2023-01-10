1cf3917c3e6ffc49a5bf92fd1a6f1ab7daaf8e1c
tomsabala <tomsabala@mail.tau.ac.il>
2023-01-10 11:33:14 +0200

    made drawing function in the plane

--- examples/py_test/y_monotone_decomposision.py
+++ examples/py_test/y_monotone_decomposision.py
@@ -2,23 +2,12 @@ from build import libGeo_2D as Geo
 from examples.Plots2D import PolygonPlot
 
 
-# dots = PolygonPlot.generateRandomPolygons(center=(0, 0),
-#                                       avg_radius=10,
-#                                       irregularity=0.2,
-#                                       spikiness=0.5,
-#                                       num_vertices=20)
+dots = PolygonPlot.generateRandomPolygons(center=(0, 0),
+                                      avg_radius=10,
+                                      irregularity=0.2,
+                                      spikiness=0.5,
+                                      num_vertices=20)
 
-dots = [(-12.95, -15.60),
-        (-28.36, -7.77),
-        (-8.80, 5.66),
-        (-12.11, 24.69),
-        (2.26, 12.54),
-        (11.47, 14.70),
-        (8.85, 1.53),
-        (16.55, -6.14),
-        (6.31, -11.63),
-        (-10.28, -33.66)
-        ]
 
 points_arr = [Geo.Point(x, y) for x, y in dots]
 poly = Geo.Polygon(points_arr)
