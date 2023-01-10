b20db27b34bb1f06ee682fdc4db22a693dfe1577
tomsabala <tomsabala@mail.tau.ac.il>
2023-01-10 11:34:46 +0200

    added area of triangle function

--- 2D/Shapes_2D/Triangle2d.cpp
+++ 2D/Shapes_2D/Triangle2d.cpp
@@ -1,11 +1,47 @@
 
+#include <iostream>
 #include "Triangle2d.h"
 
-Triangle2d::Triangle2d(Shapes2D::Point2d p, Shapes2D::Point2d q, Shapes2D::Point2d t) :
-    vertex(std::vector<Shapes2D::Point2d>{p, q, t}) {}
+using namespace Shapes2D;
+
+Triangle2d::Triangle2d(Point2d p, Point2d q, Point2d t) :
+    vertex(std::vector<Point2d>{p, q, t}) {}
 
 
-std::vector<Shapes2D::Point2d> Triangle2d::getVertices()
+std::vector<Point2d> Triangle2d::getVertices()
 {
     return this->vertex;
+}
+
+Shapes2D::Point2d Triangle2d::getA()
+{
+    return this->vertex[0];
+}
+Shapes2D::Point2d Triangle2d::getB()
+{
+    return this->vertex[1];
+}
+Shapes2D::Point2d Triangle2d::getC()
+{
+    return this->vertex[2];
+}
+void Triangle2d::setA(Shapes2D::Point2d p)
+{
+    this->vertex[0]=p;
+}
+void Triangle2d::setB(Shapes2D::Point2d p)
+{
+    this->vertex[1]=p;
+}
+void Triangle2d::setC(Shapes2D::Point2d p)
+{
+    this->vertex[2]=p;
+}
+
+
+double Triangle2d::area()
+{
+    double a = 0.5 * (getA().getX()*(getB().getY() - getC().getY()) + getB().getX()*(getC().getY() - getA().getY()) + getC().getX()*(getA().getY() - getB().getY()));
+
+    return std::abs(a);
 }
--- 2D/Shapes_2D/Triangle2d.h
+++ 2D/Shapes_2D/Triangle2d.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Point2d.h"
+#include "Segment2d.h"
 
 class Triangle2d {
 public:
@@ -8,4 +9,12 @@ public:
 
     Triangle2d(Shapes2D::Point2d p, Shapes2D::Point2d q, Shapes2D::Point2d t);
     std::vector<Shapes2D::Point2d> getVertices();
+    Shapes2D::Point2d getA();
+    Shapes2D::Point2d getB();
+    Shapes2D::Point2d getC();
+    void setA(Shapes2D::Point2d p);
+    void setB(Shapes2D::Point2d p);
+    void setC(Shapes2D::Point2d p);
+    double area();
+
 };
