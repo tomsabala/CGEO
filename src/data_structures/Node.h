d5d55caa565e9f0a494a15564934022b0504fa3b
Tom Sabala <sabala144@gmail.com>
2023-12-12 10:27:55 +0200

    fixing leak memories in sbt

--- src/data_structures/Node.h
+++ src/data_structures/Node.h
@@ -2,13 +2,12 @@
 
 class Node{
 public:
-    Shapes2D::Segment2d *s;
+    Shapes2D::Segment2d s;
     int height;
     Node * left;
     Node * right;
-    explicit Node(Shapes2D::Segment2d *seg){
+    explicit Node(const Shapes2D::Segment2d& seg) : s(seg){
         height = 1;
-        s = seg;
         left = nullptr;
         right = nullptr;
     }
--- src/data_structures/SegmentBalancedTree.h
+++ src/data_structures/SegmentBalancedTree.h
@@ -5,28 +5,30 @@ class SegmentBalancedTree{
 public:
 
     Node *root = nullptr;
-    int size{};
-    Shapes2D::Point2d *sweepLine{};
+    int size;
+    Shapes2D::Point2d *sweepLine;
 
-    void insert(Shapes2D::Segment2d *);
-    void remove(Shapes2D::Segment2d *);
-    Node * search(Shapes2D::Segment2d *);
-    Node * search_p(Shapes2D::Point2d *);
+    SegmentBalancedTree();
+
+    void insert(const Shapes2D::Segment2d &);
+    void remove(const Shapes2D::Segment2d&);
+    Node * search(const Shapes2D::Segment2d &);
+    Node * search_p(const Shapes2D::Point2d&);
     void walkInOrder();
-    Node * minn() const;
-    Node * maxx() const;
+    [[nodiscard]] Node * minn() const;
+    [[nodiscard]] Node * maxx() const;
     Node * getPred(Node *);
     Node * getSucc(Node *);
 
 private:
-    Node * _insert_(Node *, Shapes2D::Segment2d *);
-    Node * _remove_(Node *, Shapes2D::Segment2d *);
-    Node * _search_(Node *, Shapes2D::Segment2d *);
-    Node * _search_p_(Node *, Shapes2D::Point2d *);
+    Node * _insert_(Node *, const Shapes2D::Segment2d &);
+    Node * _remove_(Node *, const Shapes2D::Segment2d&);
+    Node * _search_(Node *, const Shapes2D::Segment2d &);
+    Node * _search_p_(Node *, const Shapes2D::Point2d &);
     void _walkInOrder_(Node *);
     static int _height_(Node *);
     static Node * _rightRotation_(Node *);
     static Node * _leftRotation_(Node *);
-    bool _le_(Shapes2D::Segment2d *, Shapes2D::Segment2d *) const;
+    [[nodiscard]] bool _le_(const Shapes2D::Segment2d &, const Shapes2D::Segment2d &) const;
     void _items_(std::vector<Node *> *, Node *);
 };
